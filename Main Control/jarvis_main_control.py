3fb5dec231a51d0e16e5da02cb472b7525b97c00
pryrotech <colbypry@gmail.com>
2020-06-06 21:33:47 -0230

    added if statement to main control

--- Main Control/jarvis_main_control.py
+++ Main Control/jarvis_main_control.py
@@ -1,19 +1,18 @@
 #########################################
 #   JARVIS Main Control by Colby Pryor  #
 #########################################
-
 import speech_recognition as sr
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
+    print("JARVIS is listening:")
     audio = r.listen(source)
 
 # recognize speech using Sphinx
 try:
     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    if(r.recognize_sphinx(audio) == str("hey jarvis")):
+       print("Hi colbs")
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
