9cb4373f11a24a6705e5d36f7217696e766ddd52
pryrotech <colbypry@gmail.com>
2020-06-07 03:54:40 -0230

    Added prompt into function.

--- Main Control/jarvis_main_control.py
+++ Main Control/jarvis_main_control.py
@@ -3,24 +3,43 @@
 #########################################
 import speech_recognition as sr
 from gtts import gTTS
+import pygame
 import os
 # obtain audio from the microphone
 r = sr.Recognizer()
+r.energy_threshold = 1000
 with sr.Microphone() as source:
     print("JARVIS is listening:")
     audio = r.listen(source)
-
+    r.recognize_sphinx(audio)
 # recognize speech using Sphinx
 try:
     while(r.recognize_sphinx(audio) != "jarvis"):  
         with sr.Microphone() as source:
             print("JARVIS is listening:")
             audio = r.listen(source)
+            print(r.recognize_sphinx(audio))
     else:
-        introduction = "Hello Colby! What can I do for you?"
-        texttoaud = gTTS(text=introduction, lang='en',  slow=False)
-        texttoaud.save("intro.mp3")
-        os.system("start /B intro.mp3")
+        def intro():
+            import time
+            introduction = "Hello Colby! What can I do for you?"
+            texttoaud = gTTS(text=introduction, lang='en-us')
+            texttoaud.save("intro.mp3")
+            pygame.mixer.init()
+            pygame.mixer.music.load("intro.mp3")
+            pygame.mixer.music.play()
+            with sr.Microphone() as source:
+                print("JARVIS is listening:")
+                time.sleep(2)
+                audio = r.listen(source)
+                print("JARVIS is listening:")
+            if(r.recognize_sphinx(audio) == "weather"):
+                print(r.recognize_sphinx(audio))
+                print("hello")
+            else:
+                print(r.recognize_sphinx(audio))
+                print("hi")
+        intro()
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
