fa38c7698f11b3ecfbf88132ecd38f6bd58fb665
pryrotech <colbypry@gmail.com>
2020-06-06 23:51:02 -0230

    added more validation and text-to-speech

--- Main Control/jarvis_main_control.py
+++ Main Control/jarvis_main_control.py
@@ -2,6 +2,8 @@
 #   JARVIS Main Control by Colby Pryor  #
 #########################################
 import speech_recognition as sr
+from gtts import gTTS
+import os
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -10,9 +12,15 @@ with sr.Microphone() as source:
 
 # recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-    if(r.recognize_sphinx(audio) == str("hey jarvis")):
-       print("Hi colby")
+    while(r.recognize_sphinx(audio) != "jarvis"):  
+        with sr.Microphone() as source:
+            print("JARVIS is listening:")
+            audio = r.listen(source)
+    else:
+        introduction = "Hello Colby! What can I do for you?"
+        texttoaud = gTTS(text=introduction, lang='en',  slow=False)
+        texttoaud.save("intro.mp3")
+        os.system("start /B intro.mp3")
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
