02945de96061d525e9d0bd2663d6eab4598999bc
pryrotech <colbypry@gmail.com>
2020-06-08 21:44:18 -0230

    Updated audio files.

--- Main Control/jarvis_main_control.py
+++ Main Control/jarvis_main_control.py
@@ -6,8 +6,6 @@ from gtts import gTTS
 import pygame
 import time
 import os
-#lists to recognize commands
-light_list = ['lights','light','the light','on','ah']
 # obtain audio from the microphone
 r = sr.Recognizer()
 r.energy_threshold = 1000
@@ -26,48 +24,49 @@ try:
                 r.energy_threshold = 1000
                 speakwords = "Hello Colby! What can I do for you?"
                 texttoaud = gTTS(text=speakwords, lang='en-us')
-                texttoaud.save("speakwords1.mp3")
+                texttoaud.save(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords1.mp3")
                 pygame.mixer.init()
-                pygame.mixer.music.load("speakwords1.mp3")
+                pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords1.mp3")
                 pygame.mixer.music.play()
                 time.sleep(float(2.3))
-                pygame.mixer.music.load("beep_tone.mp3")
+                pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\beep_tone.mp3")
                 pygame.mixer.music.play()
                 with sr.Microphone() as source:
-                    time.sleep(2)
-                    print("JARVIS is listening:")
-                    audio = r.listen(source)
-            if(r.recognize_sphinx(audio) in light_list):
-                r.energy_threshold = 1000
-                speakwords = "Would you like them on or off?"
-                texttoaud = gTTS(text=speakwords, lang='en-us')
-                texttoaud.save("speakwords3.mp3")
-                pygame.mixer.init()
-                pygame.mixer.music.load("speakwords3.mp3")
-                pygame.mixer.music.play()
-                time.sleep(float(2.3))
-                pygame.mixer.music.load("beep_tone.mp3")
-                pygame.mixer.music.play()
-                with sr.Microphone() as source:
-                    time.sleep(2)
-                    print("JARVIS is listening:")
-                    audio = r.listen(source)
+                        print("JARVIS is listening:")
+                        audio = r.listen(source)
+                light_list = ['lights','light','the light','on','ah']
                 if(r.recognize_sphinx(audio) in light_list):
-                    r.energy_threshold = 1000
-                    speakwords = "Turning on the light"
-                    texttoaud = gTTS(text=speakwords, lang='en-us')
-                    texttoaud.save("speakwords3.mp3")
-                    pygame.mixer.init()
-                    pygame.mixer.music.load("speakwords3.mp3")
-                    pygame.mixer.music.play()
-                elif(r.recognize_sphinx(audio) == "off"):
-                    r.energy_threshold = 1000
-                    speakwords = "Turning on the light"
-                    texttoaud = gTTS(text=speakwords, lang='en-us')
-                    texttoaud.save("speakwords4.mp3")
-                    pygame.mixer.init()
-                    pygame.mixer.music.load("speakwords4.mp3")
-                    pygame.mixer.music.play()
+                        r.energy_threshold = 1000
+                        speakwords = "Would you like them on or off?"
+                        texttoaud = gTTS(text=speakwords, lang='en-us')
+                        texttoaud.save(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords2.mp3")
+                        pygame.mixer.init()
+                        pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords2.mp3")
+                        pygame.mixer.music.play()
+                        time.sleep(float(2.3))
+                        pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\beep_tone.mp3")
+                        pygame.mixer.music.play()
+                        with sr.Microphone() as source:
+                            print("JARVIS is listening:")
+                            audio = r.listen(source)
+                        if(r.recognize_sphinx(audio) == 'on'):
+                            r.energy_threshold = 1000
+                            speakwords = "Turning on the light"
+                            texttoaud = gTTS(text=speakwords, lang='en-us')
+                            texttoaud.save(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords3.mp3")
+                            pygame.mixer.init()
+                            pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords3.mp3")
+                            pygame.mixer.music.play()
+                        elif(r.recognize_sphinx(audio) == 'off'):
+                            r.energy_threshold = 1000
+                            speakwords = "Turning off the light"
+                            texttoaud = gTTS(text=speakwords, lang='en-us')
+                            texttoaud.save(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords4.mp3")
+                            pygame.mixer.init()
+                            pygame.mixer.music.load(r"C:\Users\colby\OneDrive\Desktop\J.A.R.V.I.S\Main Control\audio\speakwords4.mp3")
+                            pygame.mixer.music.play()
+            if(r.recognize_sphinx(audio) == "hello"):
+                print("hi")
         else:
             with sr.Microphone() as source:
                 print("JARVIS is listening:")
